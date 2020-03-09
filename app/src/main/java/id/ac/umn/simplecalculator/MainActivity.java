c4143ae7fd9fdb662ddee6773dd58310e1edcd69
Voincier <48521065+Voincier@users.noreply.github.com>
2020-03-10 00:00:25 +0700

    Update MainActivity.java

--- app/src/main/java/id/ac/umn/simplecalculator/MainActivity.java
+++ app/src/main/java/id/ac/umn/simplecalculator/MainActivity.java
@@ -1,49 +1,33 @@
-package id.ac.umn.simplecalculator;
-
-import androidx.appcompat.app.AppCompatActivity;
+package umn.ac.id.simplecalculator;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
     public Button one,two,three,four,five,six,seven,eight,nine,zero;
     public Button btnAdd,btnSubtr,btnMult,btnDiv;
-    public Button btnCe,btnC,btnBack,btnNeg,btnDec,btnEq;
+    public Button btnDel,btnCe,btnC,btnBack,btnNeg,btnDec,btnEq;EditText EditText;
     public TextView resultText;
     public String totalText;
-    public float total=0;
-    public float prevOp,currOp;
     public char operator=' ';
+
+    float mValueOne, mValueTwo;
+
+    boolean Addition, mSubtract, Multiplication, Division, PlusMin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //number btns
-        initNumBtn();
-
-        //operator btns
-        initOpBtn();
-
-        //function btns
-        btnCe=findViewById(R.id.ce);
-        btnC=findViewById(R.id.c);
-        btnBack=findViewById(R.id.back);
-        btnNeg=findViewById(R.id.negative);
-        btnDec=findViewById(R.id.dec);
-
-        //equals ehe
-        btnEq=findViewById(R.id.equal);
-
-        //text in screen
-        resultText=(TextView) findViewById(R.id.show);
-
-        //digits
         one.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
@@ -116,47 +100,127 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
+/*        buttonAdd = (Button) findViewById(R.id.buttonadd);
+        buttonSub = (Button) findViewById(R.id.buttonsub);
+        buttonMul = (Button) findViewById(R.id.buttonmul);
+        buttonDivision = (Button) findViewById(R.id.buttondiv);
+        buttonC = (Button) findViewById(R.id.buttonC);
+        buttonCE = (Button) findViewById(R.id.buttonCE);
+        buttonEqual = (Button) findViewById(R.id.buttoneql);
+        buttonDel = (Button) findViewById(R.id.buttondel);
+        buttonPlusMin = (Button) findViewById(R.id.buttonplusmin);*/
+        EditText = (EditText) findViewById(R.id.edt1);
+
         btnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                hitung('+');
-
+            public void onClick(View v) {
+            if (EditText == null) {
+                EditText.setText("");
+            } else {
+                mValueOne = Float.parseFloat(EditText.getText() + "");
+                Addition = true;
+                EditText.setText(null);
+            }
             }
         });
 
         btnSubtr.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                hitung('-');
-                //resultText.setText(String.valueOf(total));
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(EditText.getText() + "");
+                mSubtract = true;
+                EditText.setText(null);
             }
         });
 
         btnMult.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                hitung('*');
-                resultText.setText(String.valueOf(total));
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(EditText.getText() + "");
+                Multiplication = true;
+                EditText.setText(null);
             }
         });
 
         btnDiv.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                hitung('/');
-                resultText.setText(String.valueOf(total));
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(EditText.getText() + "");
+                Division = true;
+                EditText.setText(null);
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str=EditText.getText().toString();
+                if (str.length() >1 ) {
+                    str = str.substring(0, str.length() - 1);
+                    EditText.setText(str);
+                }
+                else if (str.length() <=1 ) {
+                    EditText.setText("0");
+                }
+            }
+        });
+
+        btnNeg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mValueOne = Float.parseFloat(EditText.getText() + "");
+                EditText.setText(mValueOne * -1 + "");
+                PlusMin = true;
             }
         });
 
         btnEq.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                resultText.setText(String.valueOf(total));
-                total=0;
+            public void onClick(View v) {
+                mValueTwo = Float.parseFloat(EditText.getText() + "");
+
+                if (Addition == true) {
+                    EditText.setText(mValueOne + mValueTwo + "");
+                    Addition = false;
+                }
+
+                if (mSubtract == true) {
+                    EditText.setText(mValueOne - mValueTwo + "");
+                    mSubtract = false;
+                }
+
+                if (Multiplication == true) {
+                    EditText.setText(mValueOne * mValueTwo + "");
+                    Multiplication = false;
+                }
+
+                if (Division == true) {
+                    EditText.setText(mValueOne / mValueTwo + "");
+                    Division = false;
+                }
             }
         });
 
+        btnC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText.setText("");
+            }
+        });
 
+        btnCe.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText.setText("");
+            }
+        });
+
+        btnDec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText.setText(EditText.getText() + ".");
+            }
+        });
     }
 
     public void checkOp(char op,String i){
@@ -170,56 +234,5 @@ public class MainActivity extends AppCompatActivity {
             resultText.setText(null);
             resultText.setText(i);
         }
-    }
-
-    public void hitung(char op){
-        currOp=Float.parseFloat(resultText.getText().toString());
-
-        switch(op){
-            case '+':
-                operator='+';
-                total=prevOp+currOp;
-                prevOp=currOp;
-                resultText.setText(String.valueOf(total));
-                break;
-            case '-':
-                operator='-';
-                total=prevOp-currOp;
-                prevOp=currOp;
-                resultText.setText(String.valueOf(total));
-                break;
-            case '*':
-                operator='*';
-                total=prevOp*currOp;
-                prevOp=currOp;
-                resultText.setText(String.valueOf(total));
-                break;
-            case '/':
-                operator='/';
-                total=prevOp/currOp;
-                prevOp=currOp;
-                resultText.setText(String.valueOf(total));
-                break;
-        }
-    }
-
-    public void initNumBtn(){
-        one=findViewById(R.id.one);
-        two=findViewById(R.id.two);
-        three=findViewById(R.id.three);
-        four=findViewById(R.id.four);
-        five=findViewById(R.id.five);
-        six=findViewById(R.id.six);
-        seven=findViewById(R.id.seven);
-        eight=findViewById(R.id.eight);
-        nine=findViewById(R.id.nine);
-        zero=findViewById(R.id.zero);
-    }
-
-    public void initOpBtn(){
-        btnAdd=findViewById(R.id.add);
-        btnSubtr=findViewById(R.id.subtract);
-        btnMult=findViewById(R.id.multiply);
-        btnDiv=findViewById(R.id.divide);
     }
 }
